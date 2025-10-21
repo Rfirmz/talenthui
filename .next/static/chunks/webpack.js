a2a432f88b609262029f071ccf733617c03c367e
Rafael <therafaelfirme@gmail.com>
2025-10-21 00:38:26 -0700

    fixing video

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bdc6c63820a56de0"; }
+/******/ 		__webpack_require__.h = function() { return "5b04512d2574f15f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

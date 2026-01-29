b915efd833a993c6c323740ef3d1591cc508f47f
Jeff Mori <jeff@redcatsystems.com>
2026-01-28 20:39:43 -1000

    TH-2: update confirm email page function name

--- src/app/signup/confirm-email/page.tsx
+++ src/app/signup/confirm-email/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSearchParams } from 'next/navigation';
 
-export default function SignupPage() {
+export default function SignupConfirmEmailPage() {
 
   const searchParams = useSearchParams();
   const email = searchParams.get('email') ?? '';
